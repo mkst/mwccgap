8d5b80e46740368f97bc522673669e5b3c58d248
ProjectOblivion <ProjectOblivion>
2025-03-14 12:25:15 +0000

    Changed path-contetxt to src-dir

--- mwccgap.py
+++ mwccgap.py
@@ -31,14 +31,14 @@ def main() -> None:
     parser.add_argument("--asm-dir-prefix", type=Path)
     parser.add_argument("--macro-inc-path", type=Path)
     parser.add_argument("--target-encoding", type=str)
-    parser.add_argument("--path-context", type=Path)
+    parser.add_argument("--src-dir", type=Path)
 
     args, c_flags = parser.parse_known_args()
 
     as_flags = ["-G0"]  # TODO: base this on -sdatathreshold value from c_flags
 
     try:
-        with tempfile.NamedTemporaryFile(suffix=".c", dir=args.path_context) as temp_c_file:
+        with tempfile.NamedTemporaryFile(suffix=".c", dir=args.src_dir) as temp_c_file:
             c_file = args.c_file if read_from_file else Path(temp_c_file.name)
 
             if not read_from_file:
