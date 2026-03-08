0d6b5456b2d75c46aa2b8f00f9918f39d36f277a
tclamb <tclamb@users.noreply.github.com>
2026-03-08 03:27:18 -0400

    Handle both bugged and unbugged __sinit_ behaviors (#39)

--- tests/test_mwccgap.py
+++ tests/test_mwccgap.py
@@ -5,16 +5,11 @@ from mwccgap.mwccgap import replace_sinit
 
 
 class TestSinitSymbolNames(unittest.TestCase):
-    def test_unrelated(self):
-        symbol_name = "some_function"
-        result = replace_sinit(symbol_name, "tmp12234567.c", "test.cpp")
-        self.assertEqual(symbol_name, result)
-
     def test_short_filename(self):
         c_file_name = "short.cpp"
         temp_f_name = "tmp12456789.c"
-        symbol_name = f".p__sinit_{temp_f_name}".ljust(253)
-        expect_name = f".p__sinit_{c_file_name}".ljust(253)
+        symbol_name = f".p__sinit_{temp_f_name}"
+        expect_name = f".p__sinit_{c_file_name}"
 
         result = replace_sinit(symbol_name, temp_f_name, c_file_name)
         self.assertEqual(expect_name, result)
@@ -22,8 +17,26 @@ class TestSinitSymbolNames(unittest.TestCase):
     def test_long_filename(self):
         c_file_name = "extra_long_name.cpp"
         temp_f_name = "tmp12456789.c"
-        symbol_name = f"__sinit_{temp_f_name}".ljust(253)
-        expect_name = f"__sinit_{c_file_name}".ljust(253)
+        symbol_name = f"__sinit_{temp_f_name}"
+        expect_name = f"__sinit_{c_file_name}"
+
+        result = replace_sinit(symbol_name, temp_f_name, c_file_name)
+        self.assertEqual(expect_name, result)
+
+    def test_short_filename_bugged_symbols(self):
+        c_file_name = "short.cpp"
+        temp_f_name = "tmp12456789.c"
+        symbol_name = f".p__sinit_{temp_f_name.ljust(ord(temp_f_name[0]))}"
+        expect_name = f".p__sinit_{c_file_name.ljust(ord(c_file_name[0]))}"
+
+        result = replace_sinit(symbol_name, temp_f_name, c_file_name)
+        self.assertEqual(expect_name, result)
+
+    def test_long_filename_bugged_symbols(self):
+        c_file_name = "extra_long_name.cpp"
+        temp_f_name = "tmp12456789.c"
+        symbol_name = f"__sinit_{temp_f_name.ljust(ord(temp_f_name[0]))}"
+        expect_name = f"__sinit_{c_file_name.ljust(ord(c_file_name[0]))}"
 
         result = replace_sinit(symbol_name, temp_f_name, c_file_name)
         self.assertEqual(expect_name, result)
